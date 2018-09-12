595dfb15b359bd53e8f09aea2f6a25b2f2526b19
Giovanny Vizcaya <gio.vizcaya.666@gmail.com>
2018-09-12 11:25:25 -0500

    Curso Symfony4 de KNP University

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,33 +6,49 @@ use Symfony\Component\HttpFoundation\Response;
 # use Symfony\Component\Routing\Annotation\Route; NO SIRVE!! (Por ahora)
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
 
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage(){
-        return new Response('OMG! My first page already! WOOO!');
+
+        return $this->render('article/homepage.html.twig');
+        
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
-        public function show($slug)
-        {
-            $comments = [
-                'I ate a normal rock once. It did NOT taste like bacon!',
-                'Woohoo! I\'m going on an all-asteroid diet!',
-                'I like bacon too! Buy some from my site! bakinsomebacon.com',
-            ];
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
 
-            dump($slug, $this);
+        dump($slug, $this);
 
-            return $this->render('article/show.html.twig', [
-                'title' => ucwords(str_replace('-', ' ', $slug)),
-                'comments' => $comments,
-            ]);
-        }
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger){
+        // TODO - actually heart - unheart the article!
+
+        $logger->info('Article is being hearted');
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
+    }
 }
